87f01dd59e177dcf6a9a6508b526098d0ccd8146
LeviaThanSr <omarsarour@gmail.com>
2021-02-11 19:20:55 +0100

    Starting Project

---
+++ src/calculator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">hello world!</h1>
+    </div>
+  );
+};
+
+export default Calculator;
