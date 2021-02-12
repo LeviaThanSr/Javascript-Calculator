ceca56a585f578981afba8e6a7b239c42410b854
LeviaThanSr <omarsarour@gmail.com>
2021-02-12 20:37:54 +0100

    Update

--- src/calculator.js
+++ src/calculator.js
@@ -1,68 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "./ButtonElem";
+import Display from "./DisplayResult";
 
-const Calculator = () => {
-  return (
-    <div className="container">
-      <div className="card">
-        <div className="card-header text-end" id="display">
-          <h1 className="screen m-0">0</h1>
-        </div>
-        <div className="card-body row">
-          <div className="number btn btn-danger large" id="clear">
-            AC
-          </div>
-          <div className="number btn btn-secondary" id="divide">
-            /
-          </div>
-          <div className="number btn btn-secondary" id="multiply">
-            x
-          </div>
-          <div className="number btn btn-primary" id="seven">
-            7
-          </div>
-          <div className="number btn btn-primary" id="eight">
-            8
-          </div>
-          <div className="number btn btn-primary" id="nine">
-            9
-          </div>
-          <div className="number btn btn-secondary" id="subtract">
-            -
-          </div>
-          <div className="number btn btn-primary" id="four">
-            4
-          </div>
-          <div className="number btn btn-primary" id="five">
-            5
-          </div>
-          <div className="number btn btn-primary" id="six">
-            6
-          </div>
-          <div className="number btn btn-secondary" id="add">
-            +
-          </div>
-          <div className="number btn btn-primary" id="one">
-            1
-          </div>
-          <div className="number btn btn-primary" id="two">
-            2
-          </div>
-          <div className="number btn btn-primary" id="three">
-            3
-          </div>
-          <div className="number btn btn-secondary" id="decimal">
-            .
-          </div>
-          <div className="number btn btn-primary large" id="zero">
-            0
-          </div>
-          <div className="number btn btn-success large" id="equals">
-            =
-          </div>
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstNum: "0",
+      secondNum: "",
+      operatorSign: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    const value = e.target.innerText;
+
+    switch (value) {
+      case "AC":
+        this.setState({ firstNum: "0" });
+        break;
+      case ".":
+        !this.state.firstNum.includes(".")
+          ? this.setState({ firstNum: this.state.firstNum + value })
+          : this.setState({ firstNum: this.state.firstNum });
+        break;
+      default:
+        this.state.firstNum === "0"
+          ? this.setState({ firstNum: value })
+          : this.setState({ firstNum: this.state.firstNum + value });
+        break;
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="card shadow rounded">
+          <Display result={this.state.firstNum} />
+          <Button handleClick={this.handleClick} />
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Calculator;
