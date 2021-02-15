13de6b8e265d3b42518bcf482d6a332b5d688943
LeviaThanSr <omarsarour@gmail.com>
2021-02-15 23:50:30 +0100

    Added Eval

--- src/calculator.js
+++ src/calculator.js
@@ -1,31 +1,60 @@
 import React, { Component } from "react";
 import Button from "./ButtonElem";
 import Display from "./DisplayResult";
-
+/* eslint no-eval: 0 */
 class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
       outputVal: "0",
+      preVal: "",
+      operator: "",
     };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(e) {
     const value = e.target.innerText;
+    const { outputVal, operator, preVal } = this.state;
+    if (!Number.isNaN(Number(value))) {
+      if (this.state.outputVal === "0") {
+        this.setState({
+          outputVal: value,
+        });
+      } else {
+        this.setState({
+          outputVal: outputVal + value,
+        });
+      }
+      return;
+    }
     switch (value) {
       case "AC":
         this.setState({
           outputVal: "0",
+          preVal: "",
+          operator: "",
         });
         break;
-      default:
-        if (this.state.outputVal === "0") {
+      case ".":
+        if (outputVal.indexOf(".") === -1) {
           this.setState({
-            outputVal: value,
+            outputVal: outputVal + value,
+          });
+        }
+        break;
+      default:
+        if (!operator) {
+          this.setState({
+            operator: value,
+            preVal: outputVal,
+            outputVal: "",
           });
         } else {
+          const result = eval(`${preVal} ${operator} ${outputVal}`);
           this.setState({
-            outputVal: this.state.outputVal + value,
+            operator: value,
+            preVal: result,
+            outputVal: value === "=" ? result : "0",
           });
         }
         break;
@@ -38,7 +67,9 @@ class Calculator extends Component {
           <Display result={this.state.outputVal} />
           <Button handleClick={this.handleClick} />
         </div>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        <pre style={{ position: "absolute", left: 100, top: 300 }}>
+          {JSON.stringify(this.state, null, 2)}
+        </pre>
       </div>
     );
   }
