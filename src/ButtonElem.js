2e90052597d4c2d73cefad05fc2bd4ea3359e447
LeviaThanSr <omarsarour@gmail.com>
2021-02-16 18:47:05 +0100

    Added sign toggle

--- src/ButtonElem.js
+++ src/ButtonElem.js
@@ -1,134 +1,165 @@
 import React, { Component } from "react";
 class Button extends Component {
   render() {
+    const { handleClick } = this.props;
     return (
       <div className="card-body">
         <div
           className="number btn btn-danger large"
           id="clear"
-          onClick={this.props.handleClick}
+          name="AC"
+          onClick={handleClick}
         >
           AC
         </div>
-        <button
+        <div
           className="number btn btn-secondary"
-          id="percent"
-          onClick={this.props.handleClick}
+          id="sign"
+          name="negative"
+          onClick={handleClick}
         >
-          <span>%</span>
-        </button>
-        <button
+          <span name="negative" onClick={handleClick}>
+            &#177;
+          </span>
+        </div>
+        <div
           className="number btn btn-secondary"
           id="divide"
-          onClick={this.props.handleClick}
+          name="/"
+          onClick={handleClick}
         >
-          <span>÷</span>
-        </button>
-        <button
+          <span name="/" onClick={handleClick}>
+            &divide;
+          </span>
+        </div>
+        <div
           className="number btn btn-primary"
           id="seven"
-          onClick={this.props.handleClick}
+          name="7"
+          onClick={handleClick}
         >
           7
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="eight"
-          onClick={this.props.handleClick}
+          name="8"
+          onClick={handleClick}
         >
           8
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="nine"
-          onClick={this.props.handleClick}
+          name="9"
+          onClick={handleClick}
         >
           9
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-secondary"
           id="multiply"
-          onClick={this.props.handleClick}
+          name="*"
+          onClick={handleClick}
         >
-          <span>×</span>
-        </button>
-        <button
+          <span name="*" onClick={handleClick}>
+            &times;
+          </span>
+        </div>
+        <div
           className="number btn btn-primary"
           id="four"
-          onClick={this.props.handleClick}
+          name="4"
+          onClick={handleClick}
         >
           4
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="five"
-          onClick={this.props.handleClick}
+          name="5"
+          onClick={handleClick}
         >
           5
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="six"
-          onClick={this.props.handleClick}
+          name="6"
+          onClick={handleClick}
         >
           6
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-secondary"
           id="subtract"
-          onClick={this.props.handleClick}
+          name="-"
+          onClick={handleClick}
         >
-          <span>-</span>
-        </button>
-        <button
+          <span name="-" onClick={handleClick}>
+            &minus;
+          </span>
+        </div>
+        <div
           className="number btn btn-primary"
           id="one"
-          onClick={this.props.handleClick}
+          name="1"
+          onClick={handleClick}
         >
           1
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="two"
-          onClick={this.props.handleClick}
+          name="2"
+          onClick={handleClick}
         >
           2
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="three"
-          onClick={this.props.handleClick}
+          name="3"
+          onClick={handleClick}
         >
           3
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-secondary"
           id="add"
-          onClick={this.props.handleClick}
+          name="+"
+          onClick={handleClick}
         >
-          <span>+</span>
-        </button>
-        <button
+          <span name="+" onClick={handleClick}>
+            &#43;
+          </span>
+        </div>
+        <div
           className="number btn btn-primary"
           id="decimal"
-          onClick={this.props.handleClick}
+          name="."
+          onClick={handleClick}
         >
           .
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-primary"
           id="zero"
-          onClick={this.props.handleClick}
+          name="0"
+          onClick={handleClick}
         >
           0
-        </button>
-        <button
+        </div>
+        <div
           className="number btn btn-success large"
           id="equals"
-          onClick={this.props.handleClick}
+          name="="
+          onClick={handleClick}
         >
-          <span>=</span>
-        </button>
+          <span name="=" onClick={handleClick}>
+            &#61;
+          </span>
+        </div>
       </div>
     );
   }
