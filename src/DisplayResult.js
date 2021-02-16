5e3b1f851fcecf9c951667552562cad405308228
LeviaThanSr <omarsarour@gmail.com>
2021-02-16 20:33:16 +0100

    added miniDisplay

--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -4,6 +4,7 @@ class Display extends Component {
   render() {
     return (
       <div className="card-header text-end" id="display">
+        <h4>{this.props.miniDisplay}</h4>
         <h1 className="screen m-0">{this.props.result}</h1>
       </div>
     );
