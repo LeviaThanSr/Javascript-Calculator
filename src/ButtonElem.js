3b04d9a5d996779ef4719091283f6a1e674f6b9b
LeviaThanSr <omarsarour@gmail.com>
2021-02-17 03:38:17 +0100

    Update Buttons

--- src/ButtonElem.js
+++ src/ButtonElem.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 class Button extends Component {
   render() {
-    const { handleClick } = this.props;
+    const { handleClick, handleName } = this.props;
     return (
       <div className="card-body">
-        <div
+        <button
           className="number btn btn-danger large"
           id="clear"
           name="AC"
           onClick={handleClick}
         >
           AC
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-secondary"
           id="sign"
-          name="%"
-          onClick={handleClick}
+          name="LTS"
+          onClick={handleName}
         >
-          <span name="%" onClick={handleClick}>
-            &#37;
+          <span name="LTS" onClick={handleName}>
+            By
           </span>
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-secondary"
           id="divide"
           name="/"
@@ -31,32 +31,32 @@ class Button extends Component {
           <span name="/" onClick={handleClick}>
             &divide;
           </span>
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="seven"
           name="7"
           onClick={handleClick}
         >
           7
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="eight"
           name="8"
           onClick={handleClick}
         >
           8
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="nine"
           name="9"
           onClick={handleClick}
         >
           9
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-secondary"
           id="multiply"
           name="*"
@@ -65,32 +65,32 @@ class Button extends Component {
           <span name="*" onClick={handleClick}>
             &times;
           </span>
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="four"
           name="4"
           onClick={handleClick}
         >
           4
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="five"
           name="5"
           onClick={handleClick}
         >
           5
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="six"
           name="6"
           onClick={handleClick}
         >
           6
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-secondary"
           id="subtract"
           name="-"
@@ -99,32 +99,32 @@ class Button extends Component {
           <span name="-" onClick={handleClick}>
             &minus;
           </span>
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="one"
           name="1"
           onClick={handleClick}
         >
           1
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="two"
           name="2"
           onClick={handleClick}
         >
           2
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="three"
           name="3"
           onClick={handleClick}
         >
           3
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-secondary"
           id="add"
           name="+"
@@ -133,24 +133,24 @@ class Button extends Component {
           <span name="+" onClick={handleClick}>
             &#43;
           </span>
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="decimal"
           name="."
           onClick={handleClick}
         >
           .
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-primary"
           id="zero"
           name="0"
           onClick={handleClick}
         >
           0
-        </div>
-        <div
+        </button>
+        <button
           className="number btn btn-success large"
           id="equals"
           name="="
@@ -159,7 +159,7 @@ class Button extends Component {
           <span name="=" onClick={handleClick}>
             &#61;
           </span>
-        </div>
+        </button>
       </div>
     );
   }
