40a0f16bfb4d6d66f1f0be6eac6268f31b928b7c
LeviaThanSr <omarsarour@gmail.com>
2021-02-17 03:38:05 +0100

    Update Display

--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class Display extends Component {
   render() {
+    const { miniDisplay, Display } = this.props;
     return (
       <div className="card-header text-end">
-        <h4 className="miniScreen">{this.props.miniDisplay}</h4>
+        <h4 className="miniScreen">{miniDisplay}</h4>
         <h1 id="display" className="Screen m-0">
-          {this.props.Display}
+          {Display}
         </h1>
       </div>
     );
